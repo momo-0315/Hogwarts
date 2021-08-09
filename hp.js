46c42352f44886f25faff929cfcdb3163e1a1c71
momo-0315 <momo1777@asagi.waseda.jp>
2021-08-10 05:28:34 +0900

    ロゴを追加、jsのfor文のエラー解消（i<9だった）

--- hp.js
+++ hp.js
@@ -1,19 +1,13 @@
 const header_top = document.getElementById('header_top');//ヘッダー上部のid取得
 const header_bottom = document.getElementById('header_bottom');//ヘッダー下部のid取得
+const header = document.getElementsByClassName('header')[0];
 
-header_top.addEventListener('mousemove', () => { //ヘッダーに上部カーソルが乗るとヘッダーを表示
+header.addEventListener('mousemove', () => { //ヘッダーにカーソルが乗るとヘッダーを表示
     header_top.style.background = "#242633";
     header_bottom.style.background = "#10141b";
 });
-header_top.addEventListener('mouseleave', () => { //ヘッダー上部からカーソルが外れるとヘッダーを非表示
-    header_top.style.background = "none";
-    header_bottom.style.background = "none";
-});
-header_bottom.addEventListener('mousemove', () => { //ヘッダー下部にカーソルが乗るとヘッダーを表示
-    header_top.style.background = "#242633";
-    header_bottom.style.background = "#10141b";
-});
-header_bottom.addEventListener('mouseleave', () => { //ヘッダー下部からカーソルが外れるとヘッダーを非表示
+
+header.addEventListener('mouseleave', () => { //ヘッダーからカーソルが外れるとヘッダーを非表示
     header_top.style.background = "none";
     header_bottom.style.background = "none";
 });
@@ -32,7 +26,7 @@ for (let i = 0; i < 4; i++) { //三角マークをクリックするとと回転
     });
 };
 
-for (let i = 0; i < 10; i++) { //ヘッダー下部のリストにカーソルを当てると下線が引かれる
+for (let i = 0; i < 9; i++) { //ヘッダー下部のリストにカーソルを当てると下線が引かれる
     let header_bottom_list_box = document.getElementsByClassName('header_bottom_list_box')[i];
     header_bottom_list_box.addEventListener('mousemove', () => {
         header_bottom_list_box.classList.add('header_bottom_list_underline');
